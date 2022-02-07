953952c1ac73c7394f3b15a434e25b6ec1867edd
Ash Bridges <ashbridges@virginmedia.com>
2022-02-07 20:44:53 +0000

    updated projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -14,7 +14,7 @@ import '../styles/styles.scss'
 
 function Projects({ setProject }) {
     const [openPrestige, setOpenPrestige] = useState(null);
-    const [openMarket, setOpenMarket] = useState(null);
+    const [openMarquet, setOpenMarquet] = useState(null);
     const [openChef, setOpenChef] = useState(null);
     const [openPyre, setOpenPyre] = useState(null);
     const [openIllume, setOpenIllume] = useState(null);
@@ -31,30 +31,35 @@ function Projects({ setProject }) {
                 <p>Check out some of the projects I've been working on...</p>
             </div>
             <div className="projects" data-aos="fade-up">
-                    <div className="project focus">
+                    <div className="project">
                         <img src={travel} alt="Prestige" />
                         <h1>Prestige</h1>
                         <p>Single page travel website/landing page</p>
+                        <button className="focus" onClick={() => { setOpenPrestige(true) }}>View</button>
                     </div>
-                    <div className="project focus">
+                    <div className="project">
                         <img src={shop} alt="Market" />
                         <h1>Marquet</h1>
                         <p>Multipage ecommerce marketplace</p>
+                        <button className="focus" onClick={() => { setOpenMarquet(true) }}>View</button>
                     </div>
-                    <div className="project focus">
+                    <div className="project">
                         <img src={chef} alt="Chef" />
                         <h1>Chef</h1>
                         <p>Recipe search app connected to an API</p>
+                        <button className="focus" onClick={() => { setOpenChef(true) }}>View</button>
                     </div>
-                    <div onClick={() => { setOpenPyre(true) }} className="project focus">
+                    <div className="project">
                         <img src={pyre} alt="Pyre" />
                         <h1>Pyre</h1>
                         <p>Music player app with a simple dashboard layout</p>
+                        <button className="focus" onClick={() => { setOpenPyre(true) }}>View</button>
                     </div>
-                    <div className="project focus">
+                    <div className="project">
                         <img src={illume} alt="Illume" />
                         <h1>Illume</h1>
                         <p>Social media app aimed at helping creatives</p>
+                        <button className="focus" onClick={() => { setOpenIllume(true) }}>View</button>
                     </div>
             </div>
         </div>
