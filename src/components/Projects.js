23f9c345190df852d614a5c27f9e0d80bf99444f
Ash Bridges <ashbridges@virginmedia.com>
2022-03-15 12:46:30 +0000

    updated projects component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import Aos from 'aos';
 
+import MarquetModal from './MarquetModal';
+import EatzModal from './EatzModal';
 import PyreModal from './PyreModal';
+import IllumeModal from './IllumeModal';
+import TravelModal from './TravelModal';
 
 import travel from '../assets/travel.svg';
 import chef from '../assets/chef.svg';
@@ -12,10 +16,10 @@ import illume from '../assets/illume.svg';
 import "aos/dist/aos.css";
 import '../styles/styles.scss'
 
-function Projects({ setProject }) {
-    const [openPrestige, setOpenPrestige] = useState(null);
+function Projects() {
+    const [openTravel, setOpenTravel] = useState(null);
     const [openMarquet, setOpenMarquet] = useState(null);
-    const [openChef, setOpenChef] = useState(null);
+    const [openEatz, setOpenEatz] = useState(null);
     const [openPyre, setOpenPyre] = useState(null);
     const [openIllume, setOpenIllume] = useState(null);
 
@@ -27,27 +31,27 @@ function Projects({ setProject }) {
         <div>
             <div className="projects__section" id="projects">
             <div className="projects__header" data-aos="fade-right" data-aos-anchor-placement="top-center"  >
-                <h1 data-aos="fade-right" >PORTFOLIO.</h1>
-                <p data-aos="fade-right" data-aos-delay="200" >Check out some of the projects I've been working on...</p>
+                <h1 data-aos="fade-right">PORTFOLIO.</h1>
+                <p data-aos="fade-right" data-aos-delay="200" >Check out some of the <strong>projects</strong> I've been working on...</p>
             </div>
             <div className="projects" data-aos="fade-up">
                     <div className="project">
-                        <img src={travel} alt="Prestige" />
-                        <h1>Prestige</h1>
-                        <p>Single page travel website/landing page</p>
-                        <button className="focus" onClick={() => { setOpenPrestige(true) }}>View</button>
+                        <img src={travel} al="Travel" />
+                        <h1>Travair</h1>
+                        <p>Responsive single page travel website/landing page</p>
+                        <button className="focus" onClick={() => { setOpenTravel(true) }}>View</button>
                     </div>
                     <div className="project">
                         <img src={shop} alt="Market" />
                         <h1>Marquet</h1>
-                        <p>Multipage ecommerce marketplace</p>
+                        <p>Multipage ecommerce marketplace with cart</p>
                         <button className="focus" onClick={() => { setOpenMarquet(true) }}>View</button>
                     </div>
                     <div className="project">
                         <img src={chef} alt="Chef" />
-                        <h1>Chef</h1>
-                        <p>Recipe search app connected to an API</p>
-                        <button className="focus" onClick={() => { setOpenChef(true) }}>View</button>
+                        <h1>Eatz</h1>
+                        <p>Blog-type recipe app connected to an external API</p>
+                        <button className="focus" onClick={() => { setOpenEatz(true) }}>View</button>
                     </div>
                     <div className="project">
                         <img src={pyre} alt="Pyre" />
@@ -63,7 +67,11 @@ function Projects({ setProject }) {
                     </div>
             </div>
         </div>
+        {openTravel && <TravelModal openTravel={openTravel} setOpenTravel={setOpenTravel} /> }
+        {openMarquet && <MarquetModal openMarquet={openMarquet} setOpenMarquet={setOpenMarquet} /> }
+        {openEatz && <EatzModal openEatz={openEatz} setOpenEatz={setOpenEatz} /> }
         {openPyre && <PyreModal openPyre={openPyre} setOpenPyre={setOpenPyre} /> }
+        {openIllume && <IllumeModal openIllume={openIllume} setOpenIllume={setOpenIllume} /> }
         </div>
     )
 }
